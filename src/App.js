82295d77fb1c4d8896054522786c91ee6ba0c922
vvvarshith <vvvarshith.nagubandi@gmail.com>
2024-08-04 14:01:02 +0530

    reactrouting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Contact from "./Contact";
+import About from "./About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <Link to="/home">
+            <li>HOME</li>
+          </Link>
+          <Link to="/about">
+            <li>ABOUT</li>
+          </Link>
+          <Link to="/contact">
+            <li>CONTACT</li>
+          </Link>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>This is HOME PAGE</h1>
+    </div>
+  );
+};
+
+export default Home;
